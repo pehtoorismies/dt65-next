fdb94349fee618e3e4f141861e6c595005b7b45b
pehtoorismies <pehtoorismies@gmail.com>
2022-01-04 18:39:25 +0200

    test(unit): sanity test for api

--- src/views/auth/services/__tests__/auth-service.test.ts
+++ src/views/auth/services/__tests__/auth-service.test.ts
@@ -2,25 +2,26 @@ import * as E from 'fp-ts/Either'
 
 import { authService } from '../auth-service'
 
-// const mockFetchLoginSuccess = async (url: string) => {
-//   switch (url) {
-//     case '/api/login': {
-//       return {
-//         ok: true,
-//         status: 200,
-//         json: async () => ({
-//           accessToken: 'someToken',
-//           idToken: 'someIdToken',
-//           expiresIn: '123',
-//         }),
-//       }
-//     }
-//     default: {
-//       throw new Error(`Unhandled request: ${url}`)
-//     }
-//   }
-// }
-const mockForgotPassword400 = async (url: string) => {
+const mockSuccess = async (url: string) => {
+  switch (url) {
+    case '/api/login': {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => ({
+          type: 'success',
+          accessToken: 'someToken',
+          idToken: 'someIdToken',
+          expiresIn: '123',
+        }),
+      }
+    }
+    default: {
+      throw new Error(`Unhandled request: ${url}`)
+    }
+  }
+}
+const mockError = async (url: string) => {
   switch (url) {
     case '/api/forgot-password': {
       return {
@@ -40,9 +41,33 @@ const mockForgotPassword400 = async (url: string) => {
 }
 
 describe('Api', () => {
+  describe('login', () => {
+    it('200 auth data', async () => {
+      window.fetch = jest.fn().mockImplementation(mockSuccess)
+      const login = authService.loginTask({
+        email: 'some@email.com',
+        password: '123123',
+      })
+      const response = await login()
+
+      if (E.isLeft(response)) {
+        throw new Error('Fail test')
+      }
+
+      const { right: result } = response
+      if (result.type === 'error') {
+        throw new Error('Fail test')
+      }
+
+      expect(result.accessToken).toBe('someToken')
+      expect(result.idToken).toBe('someIdToken')
+      expect(result.expiresIn).toBe('123')
+    })
+  })
+
   describe('forgotPassword', () => {
     it('400 error', async () => {
-      window.fetch = jest.fn().mockImplementation(mockForgotPassword400)
+      window.fetch = jest.fn().mockImplementation(mockError)
       const forgotPassword = authService.forgotPasswordTask({
         email: 'some@email.com',
       })
